409f389d7ff1630a3ac039e06c6de2907e434186
mbkorecki <mbokorecki@gmail.com>
2026-03-13 11:31:40 +0100

    fighting google

--- scholar_update.py
+++ scholar_update.py
@@ -4,8 +4,9 @@ import json
 scholar_id = "boCYnOcAAAAJ"
 
 author = scholarly.search_author_id(scholar_id)
-author = scholarly.fill(author)
+author = scholarly.fill(author, sections=["basics","indices","counts","publications"])
 
+# stats
 stats = {
     "citations": author["citedby"],
     "hindex": author["hindex"],
@@ -15,22 +16,29 @@ stats = {
 with open("scholar-stats.json","w") as f:
     json.dump(stats,f,indent=2)
 
-pubs=[]
 
-for pub in author["publications"][:30]:
-    p = scholarly.fill(pub)
+# publications (NO fill() calls)
+pubs = []
+
+for pub in author["publications"][:20]:
+
+    bib = pub["bib"]
+
     pubs.append({
-        "title": p["bib"].get("title",""),
-        "authors": p["bib"].get("author",""),
-        "venue": p["bib"].get("venue",""),
-        "year": p["bib"].get("pub_year","")
+        "title": bib.get("title",""),
+        "authors": bib.get("author",""),
+        "venue": bib.get("venue",""),
+        "year": bib.get("pub_year","")
     })
+
 
 with open("publications.json","w") as f:
     json.dump(pubs,f,indent=2)
 
-years=[]
-counts=[]
+
+# citation history
+years = []
+counts = []
 
 for y,c in author["cites_per_year"].items():
     years.append(y)
